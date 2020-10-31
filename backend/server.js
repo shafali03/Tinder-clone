3d16be0a41d6747084cbc6386b9fbbfc259ed7bf
shafali03 <shaf03sm@gmail.com>
2020-10-31 20:22:10 +0000

    Created a connection with mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+const connection_url = `mongodb+srv://shaf1234:alisakina110614@cluster0.gh4fh.mongodb.net/tinderdb?retryWrites=true&w=majority`
 
 
 // App Config
@@ -9,6 +10,11 @@ const port = process.env.PORT || 8001
 // Middleware
 
 // DB config
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+})
 
 // API Endpoints
 app.get('/', (req, res) => res.status(200).send('TESTING'))
